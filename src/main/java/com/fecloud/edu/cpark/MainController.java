2e05d035129b2906d89c4af6a7a7f28a5795ba1d
shinW <shinwook0330@gmail.com>
2022-05-24 21:10:24 +0900

    메인컨트롤러

--- src/main/java/com/fecloud/edu/cpark/MainController.java
+++ src/main/java/com/fecloud/edu/cpark/MainController.java
@@ -3,7 +3,7 @@ package com.fecloud.edu.cpark;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller 
 public class MainController {
 	
 	@RequestMapping("/front")
